3d74badd60135b87f20109e7c31ffff857a472d5
Math <math@pop-os.localdomain>
2022-10-18 20:08:47 -0300

    inicio

---
+++ src/Services/DB.js
@@ -0,0 +1,3 @@
+// Aqui vai o link da rota principal da rota da api para ser usada como referencia
+
+/* const API = 'localhost:4000' */
---
+++ src/Services/SignIn-SignUp/SignIn.js
@@ -0,0 +1,6 @@
+// aqui vai importar a rota principal do db e vai ser usada somente para rota de login
+
+/* Exemplo
+import API from ../DB.js
+const Login = axios.get(API + `/signin`)
+*/
