b0683f3844c6336797f10e3758f3084911fe74b8
Math <math@pop-os.localdomain>
2022-10-18 20:40:37 -0300

    arquitetura

---
+++ src/Routes/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+export default function App() {
+
+    return (
+        <BrowserRouter>
+            <section>
+                <h1>Teste</h1>
+            </section>
+            <Routes>
+
+            </Routes>
+        </BrowserRouter>
+    )
+}
