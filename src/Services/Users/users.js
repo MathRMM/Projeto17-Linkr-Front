0e445b85d8c4aba409ef5f9b3859a8329eec8eff
Math <math@pop-os.localdomain>
2022-10-20 18:54:32 -0300

    Rota de busca do usuario

--- src/Services/Users/users.js
+++ src/Services/Users/users.js
@@ -8,8 +8,8 @@ async function getUser(id, token){
 }
 
 function getSearchUsers(username, token){
-    const config = {authorization: 'Bearer ' + token}
-    return axios.get(API + `/users/search/${username}`, config)
+    const config = {authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMsImlhdCI6MTY2NjI5NTExOSwiZXhwIjoxNjY2Mjk1NzE5fQ.oMQ0q8We1D7dRpdQNJHhC1S6zT1NZhP7vAWXpn-cIyY' }
+    return axios.get(API + `/users/search/${username}`, {headers: config})
 }
 
 export {
